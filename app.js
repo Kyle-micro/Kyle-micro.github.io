436c5443e6c77b342388d040df6b06f2052055e7
kyle-micro&portfolio <youcheng.lee6@gmail.com>
2023-04-30 02:38:44 +0800

    add Frontend Web

--- app.js
+++ app.js
@@ -6,17 +6,27 @@ document.getElementById('submitDate').addEventListener('click', function() {
         const formattedDate = selectedDate.split('-').join('-');
         const targetHtml = 'Network_html/' + formattedDate + '.html';
 
-        // 使用iframe載入HTML
-        const iframe = document.createElement('iframe');
-        iframe.src = targetHtml;
-        iframe.width = '100%';
-        iframe.height = '100%';
-        iframe.style.border = 'none';
+        // 使用 XMLHttpRequest 載入HTML
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const content = document.getElementById('content');
+                content.innerHTML = xhr.responseText;
 
-        const content = document.getElementById('content');
-        content.innerHTML = '';
-        content.appendChild(iframe);
+                // 在此處插入一個新的 <script> 標籤以執行HTML中的JavaScript代碼
+                const script = document.createElement('script');
+                script.innerHTML = content.querySelector('script').innerHTML;
+                content.appendChild(script);
+            } else if (xhr.readyState === 4) {
+                alert('找不到指定日期的檔案');
+            }
+        };
+        xhr.open('GET', targetHtml, true);
+        xhr.send();
     } else {
         alert('請選擇一個日期');
     }
 });
+
+
+
