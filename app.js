b9ffdb04b8d8d2d1ef7884bf51811912b9ca79c3
kyle-micro&portfolio <youcheng.lee6@gmail.com>
2023-04-30 02:48:44 +0800

    add Frontend Web

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ document.getElementById('submitDate').addEventListener('click', function() {
         const formattedDate = selectedDate.split('-').join('-');
         const targetHtml = 'Network_html/' + formattedDate + '.html';
 
-        // 使用 window.open() 在新窗口中打开HTML文件
-        window.open(targetHtml, '_blank');
+        // 计算新窗口的大小和位置
+        const width = window.innerWidth * 0.9;
+        const height = window.innerHeight * 0.9;
+        const left = (window.innerWidth - width) / 2;
+        const top = (window.innerHeight - height) / 2;
+
+        // 使用 window.open() 在新窗口中打开HTML文件，并设置窗口的大小和位置
+        window.open(
+            targetHtml,
+            '_blank',
+            `width=${width},height=${height},left=${left},top=${top}`
+        );
     } else {
         alert('請選擇一個日期');
     }
 });
+
 
 
